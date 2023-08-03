505b2a0079504cbee9479074c1de006b28ce8a95
Liban omer <root@Boss-Man>
2023-08-03 23:53:31 +0100

    Stop tracking .env

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module myproject
 go 1.12
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	golang.org/x/crypto v0.11.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,152 +2,111 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	"golang.org/x/crypto/bcrypt"
+	"github.com/joho/godotenv" // New import
 )
 
-// Task represents a single to-do task
-type Task struct {
-	ID    int
-	Title string
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
-// User represents a user for registration and authentication
-type User struct {
-	ID       int
-	Username string
-	Password []byte
+type Response struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
 }
 
 var db *sql.DB
 
 func main() {
-	// Database connection settings
-	connStr := "postgres://postgres:Rockerz88@localhost:5432/postgres?sslmode=disable"
+	err := godotenv.Load() // Load .env file
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	// Connect to the database
-	var err error
+	connStr := os.Getenv("DATABASE_URL")
+
 	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
 
-	// Verify the database connection
-	err = db.Ping()
-	if err != nil {
+	if err = db.Ping(); err != nil {
 		log.Fatal(err)
 	}
 
-	// Create the "tasks" table if it doesn't exist
-	_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS tasks (
-			id SERIAL PRIMARY KEY,
-			title TEXT
-		);
-	`)
-	if err != nil {
+	r := mux.NewRouter()
+	r.HandleFunc("/register", registerHandler).Methods("POST")
+	r.HandleFunc("/login", loginHandler).Methods("POST")
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+
+	http.Handle("/", r)
+	if err = http.ListenAndServe(":5000", nil); err != nil {
 		log.Fatal(err)
 	}
-
-	// Create the "users" table for user registration
-	_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS users (
-			id SERIAL PRIMARY KEY,
-			username TEXT UNIQUE NOT NULL,
-			password BYTEA NOT NULL
-		);
-	`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Set up HTTP routes
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/add", addHandler)
-	http.HandleFunc("/delete", deleteHandler)
-	http.HandleFunc("/register", registerHandler) // New route for user registration
-	http.HandleFunc("/login", loginHandler)       // New code for user login
-
-	// Serve static files
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	// Start the server
-	log.Println("Server started on http://localhost:5000")
-	log.Fatal(http.ListenAndServe(":5000", nil))
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	// Fetch all tasks from the database
-	// ... (Your existing code for fetching tasks)
-}
-
-func addHandler(w http.ResponseWriter, r *http.Request) {
-	// ... (Your existing code for adding tasks)
-}
-
-func deleteHandler(w http.ResponseWriter, r *http.Request) {
-	// ... (Your existing code for deleting tasks)
 }
 
 func registerHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	// Retrieve the username and password from the form data
-	username := r.FormValue("username")
-	password := r.FormValue("password")
-
-	// Hash the password using bcrypt
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		http.Error(w, "Failed to hash password", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	// Insert the user data into the "users" table
-	_, err = db.Exec("INSERT INTO users (username, password) VALUES ($1, $2)", username, hashedPassword)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
-		http.Error(w, "Failed to insert user data", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// Redirect the user to the home page after successful registration
-	http.Redirect(w, r, "/", http.StatusFound)
+	_, err = db.Exec("INSERT INTO users (username, password) VALUES ($1, $2)", user.Username, string(hashedPassword))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response := Response{
+		Status:  "Success",
+		Message: "User created successfully!",
+	}
+
+	json.NewEncoder(w).Encode(response)
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	// Retrieve the username and password from the form data
-	username := r.FormValue("username")
-	password := r.FormValue("password")
-
-	// Fetch the user with the given username from the database
-	var dbPassword []byte
-	err := db.QueryRow("SELECT password FROM users WHERE username = $1", username).Scan(&dbPassword)
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	// Compare the stored password hash with the provided password
-	err = bcrypt.CompareHashAndPassword(dbPassword, []byte(password))
+	var dbUser User
+	err = db.QueryRow("SELECT * FROM users WHERE username=$1", user.Username).Scan(&dbUser.ID, &dbUser.Username, &dbUser.Password)
 	if err != nil {
-		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+		http.Error(w, "User not found", http.StatusNotFound)
 		return
 	}
 
-	// Successful login, redirect the user to the home page
-	http.Redirect(w, r, "/", http.StatusFound)
+	if err = bcrypt.CompareHashAndPassword([]byte(dbUser.Password), []byte(user.Password)); err != nil {
+		http.Error(w, "Invalid login credentials", http.StatusUnauthorized)
+		return
+	}
+
+	response := Response{
+		Status:  "Success",
+		Message: "Logged in successfully!",
+	}
+
+	json.NewEncoder(w).Encode(response)
 }
 
