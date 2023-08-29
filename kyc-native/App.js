8774f206c255cde6a3a1b60bb38b5af73e10f242
omer1471 <liban.omer4@gmail.com>
2023-08-29 14:09:10 +0100

    Added new loginScreen

--- kyc-native/App.js
+++ kyc-native/App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React from 'react';
+import LoginScreen from './LoginScreen';  // Import LoginScreen
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <LoginScreen />;  // Render LoginScreen
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ kyc-native/LoginScreen.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+
+export default function LoginScreen() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    const jsonData = {
+      username,
+      password,
+    };
+
+    try {
+      const response = await fetch('http://localhost:5000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(jsonData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to login');
+      }
+
+      const data = await response.json();
+      console.log(data);
+      alert('Login successful!');
+      // Navigate to the dashboard or profile page
+    } catch (error) {
+      console.error('Error:', error);
+      alert('Failed to login. Please try again later.');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+      <Image source={require('../static/images/Kealogo.jpeg')} style={styles.brandLogo} />
+      </View>
+      <Text style={styles.title}>Login to Kea</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#091723',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    marginBottom: 20,
+  },
+  brandLogo: {
+    width: 150,
+    height: 150,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    color: '#F2F8F3',
+  },
+  input: {
+    width: '80%',
+    padding: 10,
+    marginBottom: 15,
+    backgroundColor: '#F2F8F3',
+    borderRadius: 8,
+  },
+  button: {
+    width: '80%',
+    padding: 15,
+    backgroundColor: '#F2F8F3',
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#091723',
+    fontSize: 18,
+  },
+});
+
